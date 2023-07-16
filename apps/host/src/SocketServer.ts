7c179dc50dfcf27072043f2992dc6cf84f5cd490
OmGuptaIND <omgupta0720@gmail.com>
2023-07-17 03:22:29 +0530

    add: websocket upgrade sequence

--- apps/host/src/SocketServer.ts
+++ apps/host/src/SocketServer.ts
@@ -39,51 +39,115 @@ class SocketServer {
       maxPayloadLength: 16 * 1024 * 1024,
       idleTimeout: 60,
       open: async (ws: uWebSocket) => {
-        const socketId = uuid();
-        const socket: SocketType = {
-          ...ws,
-          id: socketId,
-        };
+        try {
+          const socketId = uuid();
+          const socket: SocketType = {
+            ...ws,
+            id: socketId,
+          };
 
-        const userData: UserData = ws.getUserData();
+          const userData: UserData = ws.getUserData();
 
-        logger.info({ userData }, 'New socket connection');
+          logger.info({ userData }, 'New socket connection');
 
-        this.#sockets.set(socketId, socket);
+          this.#sockets.set(socketId, socket);
 
-        this.observer.emit('open', { ws: socket });
+          this.observer.emit('open', { ws: socket });
+        } catch (error) {
+          logger.error('Error Opening Socket');
+          logger.error(error);
+        }
       },
+      upgrade: async (res: uWs.HttpResponse, req: uWs.HttpRequest, context: uWs.us_socket_context_t) => {
+        try {
+          logger.info('Upgrading Socket');
+
+          logger.info({ req, res, context });
+
+          res.onAborted(() => {
+            res.aborted = true;
+          });
+
+          res.writeStatus('101 Switching Protocols').writeHeader('test-header', 'header-value');
+
+          res.upgrade(
+            {
+              url: req.getUrl(),
+            },
+            req.getHeader('sec-websocket-key'),
+            req.getHeader('sec-websocket-protocol'),
+            req.getHeader('sec-websocket-extensions'),
+            context
+          );
+
+          if (!res.aborted) {
+            logger.info('Connection Upgraded');
+            res.cork(() => {
+              res.end('Connection Upgraded', false);
+            });
+          }
+        } catch (error) {
+          logger.error('Error Upgrading Socket: ', { req: req, res: res, context: context });
+          logger.error(error);
+        }
+      },
+
       close: async (ws: uWebSocket, code: number, message: ArrayBuffer) => {
-        const userData = ws.getUserData();
-        const socket = this.getSocket(userData.id);
+        try {
+          const userData = ws.getUserData();
+          const socket = this.getSocket(userData.id);
 
-        this.#sockets.delete(socket.id);
+          this.#sockets.delete(socket.id);
 
-        this.observer.emit('close', { ws, code, message });
+          this.observer.emit('close', { ws, code, message });
+        } catch (error) {
+          logger.error('Error Closing Socket');
+          logger.error(error);
+        }
       },
 
       ping: async (ws: uWebSocket) => {
-        const userData = ws.getUserData();
-        logger.info(`Ping from ${userData.id}`);
+        try {
+          const userData = ws.getUserData();
+          logger.info(`Ping from ${userData.id}`);
+        } catch (error) {
+          logger.error('Error Ping Socket');
+          logger.error(error);
+        }
       },
 
       pong: async (ws: uWebSocket) => {
-        const userData = ws.getUserData();
+        try {
+          const userData = ws.getUserData();
 
-        logger.info(`Pong from ${userData.id}`);
+          logger.info(`Pong from ${userData.id}`);
+        } catch (error) {
+          logger.error('Error Pong Socket');
+          logger.error(error);
+        }
       },
 
       drain: async (ws: uWebSocket) => {
-        const userData = ws.getUserData();
-        logger.info(`Drain from ${userData.id}`);
+        try {
+          const userData = ws.getUserData();
+          logger.info(`Drain from ${userData.id}`);
+        } catch (error) {
+          logger.error('Error Drain Socket');
+          logger.error(error);
+        }
       },
 
       message: async (ws: uWebSocket, message: ArrayBuffer, isBinary: boolean) => {
-        const userData = ws.getUserData();
+        try {
+          const userData = ws.getUserData();
 
-        logger.info(`Message from ${userData.id}`);
-        logger.info(message);
-        logger.info(isBinary);
+          logger.info(`Message from ${userData.id}`);
+          logger.info(message);
+          logger.info(isBinary);
+        } catch (error) {
+          logger.error('Error Message Socket: ', { msg: message });
+          logger.error(error);
+        }
       },
     });
   }
